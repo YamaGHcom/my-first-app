074f11a384e98343ea24c75b55ddac32c276d9ee
Yama VS <seiho.yamaguchi@arteryex.biz>
2024-11-07 18:50:59 +0900

    modify Dashboard.tsx ('haiku'→'task')

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,34 +1,35 @@
 import { ObjectId } from "mongodb"
 import { getCollection } from "../lib/db"
 import Link from "next/link"
-import { deleteHaiku } from "../actions/haikuController"
+import { deleteTask } from "../actions/taskController"
 
-async function getHaikus(id) {
-    const collection = await getCollection("haikus")
+//指定されたユーザーID（id）に基づいて、そのユーザーが作成したタスクをDBから取得
+async function getTasks(id: string) {
+    const collection = await getCollection("tasks")
     const results = await collection.find({ author: ObjectId.createFromHexString(id) }).sort({ _id: -1 }).toArray()
     return results
 }
 
-export default async function Dashboard(props) {
-    const haikus = await getHaikus(props.user.userId)
+type Props = {
+    user: {
+        userId: string;
+    };
+};
+
+export default async function Dashboard(props: Props) {
+    const tasks = await getTasks(props.user.userId)
 
     return (
         <div>
-            <h2 className="text-center text-2xl text-gray-600 mb-5">Your Haikus</h2>
-            {haikus.map((haiku, index) => {
+            <h2 className="text-center text-2xl text-gray-600 mb-5">Your Tasks</h2>
+            {tasks.map((task, index) => {
                 return (
                     <div key={index}>
-                        {haiku.line1}
+                        {task.line1}
                         <br />
-                        {haiku.line2}
-                        <br />
-                        {haiku.line3}
-                        <br />
-                        <Link href={`/edit-haiku/${haiku._id.toString()}`}>Edit</Link>
-                        <br />
-                        <Link className="text-red-600" href={`/edit-haiku2/${haiku._id.toString()}`}>Edit2</Link>
-                        <form action={deleteHaiku}>
-                            <input name="id" type="hidden" defaultValue={haiku._id.toString()} />
+                        <Link href={`/edit-task/${task._id.toString()}`}>Edit</Link>
+                        <form action={deleteTask}>
+                            <input name="id" type="hidden" defaultValue={task._id.toString()} />
                             <button>Delete</button>
                         </form>
                         <hr />
